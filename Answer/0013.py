6624b8d250ea36ee0f2cefaa3c989a481b875937
hxlnwpu <yishengtuwei@foxmail.com>
2018-12-05 11:41:35 +0800

    correct 0013

--- Answer/0013.py
+++ Answer/0013.py
@@ -11,7 +11,7 @@ webdata=urllib.request.urlopen(url)
 soup=BeautifulSoup(webdata.read(),"html.parser")
 for link in soup.find_all("img"):
     imgurl=link.get("src")
-    imgdata=urllib.request.urlopen(imgurl)
-    img=Image.open(imgdata)
-    img.save("../Data/"+str(random.randint(0,1000))+".jpg")
+    imgdata=urllib.request.urlopen(imgurl).read()
+    with open("../test/"+imgurl[-11:],"wb") as img:
+        img.write(imgdata)
 
